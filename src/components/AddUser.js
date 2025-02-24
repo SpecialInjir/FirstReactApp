c37a9b12527f38e386436cf8ee5495395b9f56b5
Nasya <uzdennasub@gmail.com>
2025-02-24 13:29:56 +0300

    FR-8: Работа с API через axios

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -4,9 +4,9 @@ class AddUser extends React.Component{
     constructor(props){
         super(props)
         this.state ={
-            firstname: "",
-            lastname: "",
-            bio: "",
+            first_name: "",
+            last_name: "",
+            email: "",
             age:1,
             isHappy: false
         }
@@ -15,9 +15,9 @@ class AddUser extends React.Component{
     {
         return(
             <form ref={(el)=> this.myForm = el}> //некое свойство Form где будет значение формочки
-               <input placeholder="Имя"  onChange={(e)=>this.setState({firstname: e.target.value})}/>
-               <input placeholder="Фамилия" onChange={(e)=>this.setState({lastname: e.target.value})}/>
-               <textarea placeholder="Биография" onChange={(e)=>this.setState({bio: e.target.value})}></textarea>
+               <input placeholder="Имя"  onChange={(e)=>this.setState({first_name: e.target.value})}/>
+               <input placeholder="Фамилия" onChange={(e)=>this.setState({last_name: e.target.value})}/>
+               <textarea placeholder="Биография" onChange={(e)=>this.setState({email: e.target.value})}></textarea>
                <input placeholder="Возраст" onChange={(e)=>this.setState({age: e.target.value})}/>
                <label htmlFor="isHappy">Счастлив?</label>
                <input  type="checkbox" id="isHappy" onChange={(e)=>this.setState({isHappy: e.target.checked})}/>
@@ -26,9 +26,9 @@ class AddUser extends React.Component{
                     onClick={() => {
                         this.myForm.reset()
                         this.userAdd ={
-                            firstname: this.state.firstname,
-                            lastname: this.state.lastname,
-                            bio: this.state.bio,
+                            first_name: this.state.firstname,
+                            last_name: this.state.lastname,
+                            email: this.state.email,
                             age: this.state.age,
                             isHappy: this.state.isHappy,
                         }
